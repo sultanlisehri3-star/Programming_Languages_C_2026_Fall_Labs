c9e0a9ac7bd4e5b9ee349124407ab7d36ac5c696
sultanlisehri3-star <sultanlisehri3@gmail.com>
2026-09-21 10:46:28 +0000

    lab2

--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,17 +12,27 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+  long long result = 1;
+
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a non-negative integer n: ");
-    scanf("%d", &n);
+  printf("Enter a non-negative integer n: ");
+  scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
+  if (n < 0) {
+    printf("Error: n must be non-negative.\n");
+  } else {
+    long long result = factorial(n);
+    printf("%d! = %lld\n", n, result);
+  } 
 
-    return 0;
+  return 0;
 }
